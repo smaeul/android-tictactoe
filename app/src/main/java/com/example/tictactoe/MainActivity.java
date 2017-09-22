d3d0cc9212c83def24b2d15c84b1b00fef1c3aa1
Samuel Holland <samuel@sholland.org>
2017-09-22 00:45:46 -0500

    MainActivity: Hook up an instance of the controller

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
+import com.example.tictactoe.databinding.MainActivityBinding;
+
 /**
  * Main tic-tac-toe game board activity.
  */
 
 public class MainActivity extends Activity {
+    private final GameController controller = new GameController();
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.main_activity);
+        final MainActivityBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.main_activity);
+        binding.setController(controller);
     }
 }
