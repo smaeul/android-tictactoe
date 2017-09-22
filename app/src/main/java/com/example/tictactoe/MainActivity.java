b3f46a74340eb2f4492ff3ff5d02942181937199
Samuel Holland <samuel@sholland.org>
2017-09-21 22:16:32 -0500

    MainActivity: Create and use layout

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tictactoe;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 /**
@@ -11,5 +12,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.main_activity);
     }
 }
