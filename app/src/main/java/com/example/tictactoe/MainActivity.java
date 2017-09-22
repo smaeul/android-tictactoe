c2a12f2fdb3e81a409f4a9362d0bcb35f46c152e
Samuel Holland <samuel@sholland.org>
2017-09-22 00:46:20 -0500

    MainActivity: Add a class comment

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tictactoe;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * Main tic-tac-toe game board activity.
+ */
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
